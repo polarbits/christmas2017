dfe6af3eafc62be25414d368a0ae83ad8c3fa846
Maciej Korsan <maciejkorsan@gmail.com>
2017-12-21 17:13:40 +0100

    feat: routing for wishes

--- web/src/Wish.js
+++ web/src/Wish.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import SyncanoClient from 'syncano-client'
 
-const Wish = () => {
-  const s = new SyncanoClient('icy-bush-5836');
-  s.get('Christmas2017/get-wishes/', {wish: '6538'})
-  .then(wish => {
-    console.log(wish)
-  })
-  return (
-    <div className="container">
-      <h1>Home!!</h1>
-    </div>
-  );
+class Wish extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      wish: ''
+    }
+  }
+
+  componentDidMount() { 
+    const wishId = this.props.match.params.id;
+    const s = new SyncanoClient('icy-bush-5836'); 
+    s.get('Christmas2017/get-wishes/', {wish: wishId})
+    .then(items => {
+      this.setState({wish: items.wishes[0]});
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>{ this.state.wish.sender }</h1>
+      </div>
+    );
+  }
 }
 
 export default Wish;
