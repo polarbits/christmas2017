8a5fd6d6efd99f6a2debbf473bf4aed635abc751
grzegorz <grzegorz@polarbits.co>
2017-12-21 16:33:39 +0100

    feat: client calls endpoint

--- web/src/Wish.js
+++ web/src/Wish.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import SyncanoClient from 'syncano-client'
 
 const Wish = () => {
+  const s = new SyncanoClient('icy-bush-5836');
+  s.get('Christmas2017/get-wishes/', {wish: '6538'})
+  .then(wish => {
+    console.log(wish)
+  })
   return (
     <div className="container">
       <h1>Home!!</h1>
