9d72c5c0cd93694959625ad7337443eea336ab0e
grzegorz <grzegorz@polarbits.co>
2017-12-22 14:37:59 +0100

    fix: fix to merge

--- app/app/js/app.js
+++ app/app/js/app.js
@@ -1,1 +1,33 @@
 const wishes = require('./wishes.js');
+
+const url = 'https://icy-bush-5836.syncano.space/Christmas2017/post-wishes/'; 
+const button = document.querySelector('.send--js'); 
+button.addEventListener('click', (e) => { 
+  e.preventDefault(); 
+  const senderValue = document.querySelector('[name=sender]').value; 
+  const receiverValue = document.querySelector('[name=receiver]').value; 
+  const wishesValue = document.querySelector('[name=wishes]').value; 
+ 
+  const apiUrl = new URL(url),  
+    params = {sender: senderValue, receiver: receiverValue, wishes: wishesValue } 
+  Object.keys(params).forEach(key=>apiUrl.searchParams.append(key, params[key])); 
+ 
+  let cryptoId; 
+  fetch(apiUrl) 
+    .then(function(resp) { 
+      console.log(resp); 
+      return resp.json(); 
+    }) 
+    .then(function(json){ 
+      console.log(json) 
+      return json; 
+    }) 
+    .then(function(crypto){ 
+      const inputLink = document.querySelector('.form__link'); 
+      inputLink.value = 'https://xmas2017.polarbits.co/wish/#'+crypto; 
+      const form = document.querySelector('.form__form'); 
+      form.classList.add('form--disabled'); 
+      const inputLinkDiv = document.querySelector('.form__input'); 
+      inputLinkDiv.classList.remove('form--disabled'); 
+    }) 
+}); 
